dad5f2c87145e0b6359bc49cd45bd488d425ece9
Jake Finn <lof.steven@gmail.com>
2022-02-21 12:29:23 -0600

    Make logic to method that accepts some content and adds it to the db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,24 +14,17 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the database');
+  console.log('Post');
 
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  // what is the second parameter for?
+  const jateDb = await openDB('jate', '1');
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
+// whats a object store again?
+  const store = tx.objectStore('jate');
 
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('🚀 - data saved to the database', result);
-}
+  const request = store.add({ jate: content})
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
